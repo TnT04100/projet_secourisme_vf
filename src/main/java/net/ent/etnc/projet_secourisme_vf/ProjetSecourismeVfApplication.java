4f67d27d38f2b44650893ee94ed2c27bcbc5e082
tristan.keller <tristan.keller@etrs.terre.defense.gouv.fr>
2025-03-07 10:46:40 +0100

    Init Repo

---
+++ src/main/java/net/ent/etnc/projet_secourisme_vf/ProjetSecourismeVfApplication.java
@@ -0,0 +1,13 @@
+package net.ent.etnc.projet_secourisme_vf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSecourismeVfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetSecourismeVfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ent/etnc/projet_secourisme_vf/ProjetSecourismeVfApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ent.etnc.projet_secourisme_vf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSecourismeVfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
